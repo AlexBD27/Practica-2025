29d1de6137a153314d1fa34d0c2feee63af17387
AlexBD27 <109763600+AlexBD27@users.noreply.github.com>
2025-06-25 10:34:08 -0500

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Cliente (
+  dni INT64,
+  razon_social STRING(1024),
+  direccion STRING(1024),
+  telefono STRING(1024),
+  monto FLOAT64,
+  distrito INT64,
+) PRIMARY KEY(dni);
+
+CREATE TABLE Distrito (
+  codigo INT64,
+  nombre STRING(1024),
+) PRIMARY KEY(codigo);
